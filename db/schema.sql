52e3ad3cf8af901da6def0ca47e13328000c42e7
Thorson <thorsonb@nala.roche.com>
2021-10-20 13:14:42 -0700

    add employee, role, and department data to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,6 @@ CREATE TABLE employee (
 	id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    emp_dept VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+USE employee_db;
+
+INSERT INTO department(name)
+VALUES 
+("Administration"),
+("Human Resources"),
+("Marketing"),
+("Coaching Staff"),
+("Team");
+
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('CEO/Owner', 500000, 1),
+('Employee Relations', 300000, 2),
+('Marketing Director', 100000, 3),
+('Coach', 80000, 4),
+('Assistance Coach', 60000, 4),
+('Player', 40000, 5);
+
+
+
+
+
+INSERT INTO employee(first_name, last_name, salary, role_id, manager_id)
+VALUES 
+('Rebecca', 'Boss', 500000, 1, null),
+('Ted', 'Lasso', 80000, 4, 1),
+('Roy', 'Kent', 60000, 5, 4),
+('Keely', 'Jones', 100000, 3, 1),
+('Coach', 'Beard', 60000, 4, 4),
+('Higgins', 'HR', 300000, 2, 1),
+('Sam', 'Obisanya', 40000, 6, 4);
